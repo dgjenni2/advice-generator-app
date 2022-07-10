634eb0e1dfe6b5311b559ee81b0f9a6c64927fd7
dgjenni2 <digdug14@comcast.net>
2022-07-09 20:13:22 -0500

    Completed solution with API call.

--- README-template.md
+++ README-template.md
@@ -10,7 +10,7 @@ This is a solution to the [Advice generator app challenge on Frontend Mentor](ht
   - [Links](#links)
 - [My process](#my-process)
   - [Built with](#built-with)
-  - [What I learned](#what-i-learned)
+  - [What I learned](#what-i-learned)A
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
@@ -34,7 +34,7 @@ Users should be able to:
 
 Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
 
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
+Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
 
 Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
 
@@ -69,14 +69,16 @@ To see how you can add code snippets, see below:
 ```html
 <h1>Some HTML code I'm proud of</h1>
 ```
+
 ```css
 .proud-of-this-css {
-  color: papayawhip;
+	color: papayawhip;
 }
 ```
+
 ```js
 const proudOfThisFunc = () => {
-  console.log('🎉')
+	console.log("🎉")
 }
 ```
 
---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./App.css"
+import mobileDivider from "./images/pattern-divider-mobile.svg"
+import desktopDivider from "./images/pattern-divider-desktop.svg"
+import diceRerollIcon from "./images/icon-dice.svg"
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function App() {
+	const [adviceNumber, setAdviceNumber] = useState(0)
+	const [adviceText, setAdviceText] = useState(
+		"Wisdom is often found in the wise but is often learned from foolish acts."
+	)
+
+	useEffect(() => {
+		getNewAdvice()
+	}, [])
+
+	const getNewAdvice = () => {
+		axios.get("https://api.adviceslip.com/advice").then((result) => {
+			let slip = result.data.slip
+			setAdviceNumber(slip.id)
+			setAdviceText(slip.advice)
+		})
+	}
+
+	return (
+		<>
+			<main className="App">
+				<div className="advice-display">
+					<div className="container">
+						<h4 className="text-green">Advice #{adviceNumber}</h4>
+						<p className="text-light">"{adviceText}"</p>
+						<img
+							className="mobile"
+							src={mobileDivider}
+							alt="divider mobile"
+						></img>
+						<img
+							className="desktop"
+							src={desktopDivider}
+							alt="divider mobile"
+						></img>
+					</div>
+					<button
+						className="advice-generator reset-margin"
+						onClick={() => getNewAdvice()}
+					>
+						<img
+							className="advice-generator__dice reset-margin"
+							src={diceRerollIcon}
+							alt="reroll advice dice"
+						></img>
+					</button>
+				</div>
+			</main>
+			<footer class="attribution">
+				Challenge by{" "}
+				<a
+					href="https://www.frontendmentor.io?ref=challenge"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Frontend Mentor
+				</a>
+				. Coded by <a href="#">Your Name Here</a>.
+			</footer>
+		</>
+	)
+}
+
+export default App
